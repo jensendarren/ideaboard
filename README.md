1b4dfd933059b3a8bf806b7b458c9e9428f2187a
Darren Jensen <darren.jensen@gmail.com>
2018-02-02 23:03:21 +0700

    Display ideas on the dashboard

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
+To run the app using docker-compose:
+
+`dc up`
+
+To shell into the *running container*:
+
+`docker exec -it ideaboard bash`
+
 Below you will find some information on how to perform common tasks.<br>
 You can find the most recent version of this guide [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
 
--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import Idea from './Idea'
 
 class IdeasContainer extends Component {
+  constructor(props) {
+    super(props)
+    console.log("Dadouland FTW")
+    this.state = {
+      ideas: []
+    }
+  }
+  
+  componentDidMount() {
+    axios.get('http://localhost/api/v1/ideas.json')
+    .then(response => {
+      console.log("Our ideas: ", response.data)
+      this.setState({ideas: response.data})
+    })
+    .catch(error => console.log(error))
+  }  
+  
   render() {
     return (
       <div>
-        Ideas
+        {this.state.ideas.map((idea) => {
+          return (<Idea idea={idea} key={idea.id} />)
+        })}
       </div>
-    )
+    );
   }
 }
-
-export default IdeasContainer
+  
+export default IdeasContainer;
